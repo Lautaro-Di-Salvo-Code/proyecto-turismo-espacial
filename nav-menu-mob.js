e753f1fc82d583829eaeee931ddd19ee9e9b36b3
primer push <lautarodisalvo5@gmail.com>
2023-11-18 22:32:43 -0300

    correction menu

--- nav-menu-mob.js
+++ nav-menu-mob.js
@@ -12,14 +12,15 @@
             // let buttonClose = document.querySelector(".close-x svg")
             // let buttonOpen = document.querySelector(".arrow svg")
 
-            if(e.target.matches(".arrow")){
+            if(e.target.matches(".arrow") || e.target.matches(".arrow svg")){
                 console.log(e.target)
-                e.target.classList.add("button-none")
+                
+                document.querySelector(".arrow").classList.add("button-none")
                 // document.querySelector(".close-x").classList.remove("button-none")
                document.querySelector(".nav-style-mobile").classList.remove("off")
                 
             }
-            if(e.target.matches(".close-x") ){
+            if(e.target.matches(".close-x") || e.target.matches(".close-x svg") ){
                 document.querySelector(".arrow").classList.remove("button-none")
                 // e.target.classList.add(".button-none")
                 document.querySelector(".nav-style-mobile").classList.add("off")
